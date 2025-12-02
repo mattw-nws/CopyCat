04f4a17a58274a9576d26c7591cfb8577d05ad3a
Matt Williamson <87771120+mattw-nws@users.noreply.github.com>
2025-12-02 14:48:07 +0000

    Moved NaN handling out of get_dataset

--- src/copycatbmi/bmi.py
+++ src/copycatbmi/bmi.py
@@ -88,9 +88,12 @@ class CopyCat(BmiBase):
     def update_until(self, time: float) -> None:
         self._tN = int(time)
         ds = self._source_manager.get_dataset(self._source, self._tN)
-        logger.info(self._feature_id)
-        logger.info(ds['streamflow'][(ds['feature_id'] == self._feature_id)].values[0])
-        self._q = 3600 * ds['streamflow'][(ds['feature_id'] == self._feature_id)].values[0] / self._area_sqm
+        logger.debug(self._feature_id)
+        #logger.debug(ds['streamflow'][(ds['feature_id'] == self._feature_id)].values[0])
+        result = ds['streamflow'][(ds['feature_id'] == self._feature_id)]
+        if result.isnull():
+            logger.warning(f"Got NaN for feature {self._feature_id} at tN={self._tN}, using zero.")
+        self._q = 3600 * result.fillna(0).values[0] / self._area_sqm
         self._qvar = np.array([self._q], dtype=np.float32)
 
     def get_time_step(self) -> float:
--- src/copycatbmi/troute.py
+++ src/copycatbmi/troute.py
@@ -1,8 +1,11 @@
+import logging
 from typing import Union, Optional
 from datetime import datetime
 from pathlib import Path
 
 from .source_manager import SourceManager, Source
+
+logger = logging.getLogger(__name__)
 
 class TRouteWarmer():
 
@@ -18,7 +21,13 @@ class TRouteWarmer():
             source = sm.derive_source(t0=tm1, tend=tm1, source_base=self._source_base)
             ds = sm.get_dataset(source, 0)
 
-            qtm1_out = ds.sel(feature_id=feature_ids)['streamflow'].to_dataframe()
+            # Get count of NaNs in the `streamflow` variable and issue a warning if there are more than 0...
+            ds = ds.sel(feature_id=feature_ids)
+            nan_count = int(ds['streamflow'].isnull().sum().values)
+            if nan_count > 0:
+                logger.warning(f"Dataset has {nan_count} NaN values in `streamflow` variable. Filling with 0.")
+
+            qtm1_out = ds['streamflow'].fillna(0).to_dataframe()
             qtm1_out['flowpath_id'] = flowpath_ids
             qtm1_out = qtm1_out.set_index(['flowpath_id'], drop=True)
             qtm1_out.index.name = None
