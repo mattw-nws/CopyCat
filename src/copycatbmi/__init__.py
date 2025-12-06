46963a983500fcd76f6d83a60018842242874f96
Matt Williamson <87771120+mattw-nws@users.noreply.github.com>
2025-12-06 15:03:28 +0000

    Spurious comments removed

--- src/copycatbmi/__init__.py
+++ src/copycatbmi/__init__.py
@@ -1,10 +1,10 @@
 from .bmi import CopyCat
 from .troute import TRouteWarmer
 
-from importlib.metadata import version as _version # This should cause it to be mangled, yes?
+from importlib.metadata import version as _version
 
 try:
-    __version__ = _version("copycatbmi")  # Replace "my_package" with your actual package name
+    __version__ = _version("copycatbmi")
 except Exception:
     # Fallback for local development or when the package is not yet installed
     __version__ = "unknown"
